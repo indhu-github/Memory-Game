1ffe37ae24e257aefa5a69ac85231a17129b572e
indhu-github <indhusworkmail@gmail.com>
2021-10-30 21:21:08 +0530

    shuffling cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
+import {useState} from "react";
 import "./App.css";
 
+const cardImages=[
+  {"src":"/img/apple.jpg"},
+  {"src":"/img/custard-apple.jpg"},
+  {"src":"/img/dragon-fruit.jpg"},
+  {"src":"/img/grapes.jpg"},
+  {"src":"/img/papaya.jpg"},
+  {"src":"/img/watermelon.jpg"},
+  ]
+
 function App() {
+const [cards,setCards]=useState([]);
+const [turns,setTurns]=useState([]);
+
+//shuffle cards
+const shuffleCards=()=>{
+  const shuffledCards=[...cardImages,...cardImages]
+  .sort(()=>Math.random()-0.5)
+  .map((card)=>({...card,id:Math.random()}))
+
+  setCards(shuffledCards);
+  setTurns(0);
+}
+
+console.log(cards,turns);
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
