53b038e1dbfaf7f5980335deff58040c08c68ba2
indhu-github <indhusworkmail@gmail.com>
2021-11-01 19:35:56 +0530

    making choices

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ const cardImages = [
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   //shuffle cards
   const shuffleCards = () => {
@@ -27,6 +29,11 @@ function App() {
 
   console.log(cards, turns);
 
+  const handleChoice = (card) => {
+    console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
@@ -34,7 +41,7 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <Card key={card.id} card={card} />
+          <Card key={card.id} card={card} handleChoice={handleChoice} />
         ))}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,20 @@
 import "./Card.css";
 
-export default function Card({ card }) {
+export default function Card({ card, handleChoice }) {
+  const handleClick = () => {
+    handleChoice(card);
+  };
+
   return (
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="front card" />
-        <img className="back" src="/img/cover.jpg" alt="back card" />
+        <img
+          className="back"
+          src="/img/cover.jpg"
+          alt="back card"
+          onClick={handleClick}
+        />
       </div>
     </div>
   );
