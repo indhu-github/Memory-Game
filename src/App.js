d958ae219a94f97ed28b9f36b2aa63b74afb16b9
indhu-github <indhusworkmail@gmail.com>
2021-11-01 19:52:47 +0530

    comparing two choices

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/Card";
 
@@ -27,11 +27,31 @@ function App() {
     setTurns(0);
   };
 
-  console.log(cards, turns);
+  // console.log(cards, turns);
 
   const handleChoice = (card) => {
-    console.log(card);
+    // console.log(card);
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
+
+  //compare two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        console.log("cards match");
+        resetTurn();
+      } else {
+        console.log("cards dont match");
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  //reset choices and increment turns
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
   };
 
   return (
